f548ffa7426e43cd21be199b54fb3dc982af89ab
Rose <rodo0001@stud.kea.dk>
2024-10-28 12:17:12 +0100

    header+farver

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#64C0FF",
+        peach: "#FF6F61",
+      },
+    },
+  },
+  plugins: [],
+};
